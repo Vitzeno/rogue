4820b3506ee77feeff641c6aaa2ae8acd76fc27c
Mohamed Yusuf <vitzeno@gmail.com>
2018-12-27 16:52:15 +0000

    Add doors to struct and randomly generate door for each room

--- main.c
+++ main.c
@@ -1,19 +1,25 @@
 #include <ncurses.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define NUM_OF_ROOMS 6   
 
+typedef struct Position {
+	int x;
+	int y;
+} Position;
+
 typedef struct Player {
-	int xPos;
-	int yPos;
+	Position position;
 	int health;	
 } Player;
 
 typedef struct Room {
-	int xPos;
-	int yPos;
+	Position position;
 	int width;
 	int height;
+
+	Position doors[4];
 } Room;
 
 
@@ -27,12 +33,8 @@ int renderRoom(Room * room);
 int renderPlayer(Player * player);
 
 int handleInput(int input, Player * player);
-int checkPosition(int newyPos, int newxPos, Player * player);
-int updatePlayerPosition(int newyPos, int newxPos, Player * player);
-
-
-
-
+int checkPosition(int newPositionY, int newPositionX, Player * player);
+int updatePlayerPosition(int newPositionY, int newPositionX, Player * player);
 
 
 int main() {
@@ -77,8 +79,8 @@ Player * setUpPlayer() {
 	Player * newPlayer;
 	newPlayer = (Player * )malloc(sizeof(Player));
 
-	newPlayer->xPos = 15;
-	newPlayer->yPos = 15;
+	newPlayer->position.x = 15;
+	newPlayer->position.y = 15;
 	newPlayer->health = 3;
 
 	return newPlayer;
@@ -87,8 +89,8 @@ Player * setUpPlayer() {
 /*
 This function creates a single rooms with the defeined
 specificaions.
-@param x xpos of room
-@param y ypos of room
+@param x position.x of room
+@param y position.y of room
 @param h height of romm
 @param w width of room
 */
@@ -96,10 +98,28 @@ Room * createRoom(int x, int y, int h, int w) {
 	Room * newRoom;
 	newRoom = malloc(sizeof(Room));
 
-	newRoom->xPos = x;
-	newRoom->yPos = y;
+	newRoom->position.x = x;
+	newRoom->position.y = y;
 	newRoom->height = h;
 	newRoom->width = w;
+	srand(time(NULL));
+
+	/* top door */
+	newRoom->doors[0].y = newRoom->position.y;
+	newRoom->doors[0].x = rand () % (w - 2) + newRoom->position.x + 1; //(w - 2) reduce range by 2 to ensure no corner doors
+	
+	/* bottom door */
+	newRoom->doors[1].y = newRoom->position.y + h - 1;
+	newRoom->doors[1].x = rand () % (w - 2) + newRoom->position.x + 1;
+
+	/* left door */
+	newRoom->doors[2].y = rand () % (h - 2) + newRoom->position.y + 1;
+	newRoom->doors[2].x = newRoom->position.x;
+
+	/* right door */
+	newRoom->doors[3].y = rand () % (h - 2) + newRoom->position.y + 1;
+	newRoom->doors[3].x = newRoom->position.x + w - 1;
+
 
 	return newRoom;
 }
@@ -131,23 +151,29 @@ int renderRoom(Room * room) {
 	int y;
 
 	/* rendering top and bottom of room */
-	for(x = room->xPos; x < room->xPos + room->width; x++) {
+	for(x = room->position.x; x < room->position.x + room->width; x++) {
 		/* prints string at designated y,x location (note: y,x coordinate NOT x,y)*/
-		mvprintw(room->yPos, x, "-");
-		mvprintw(room->yPos + room->height - 1, x, "-");
+		mvprintw(room->position.y, x, "-");
+		mvprintw(room->position.y + room->height - 1, x, "-");
 	}
 
 	/* rendering interior and sides of room */
-	for(y = room->yPos + 1; y < room->yPos + room->height - 1; y++) {
+	for(y = room->position.y + 1; y < room->position.y + room->height - 1; y++) {
 		/* prints string at designated y,x location (note: y,x coordinate NOT x,y)*/
-		mvprintw(y, room->xPos, "|");
-		mvprintw(y, room->xPos + room->width - 1, "|");
+		mvprintw(y, room->position.x, "|");
+		mvprintw(y, room->position.x + room->width - 1, "|");
 		/* render interior of room */
-		for(x = room->xPos + 1;x < room->xPos + room->width - 1;x++) {
+		for(x = room->position.x + 1;x < room->position.x + room->width - 1;x++) {
 			/* prints string at designated y,x location (note: y,x coordinate NOT x,y)*/
 			mvprintw(y, x, ".");
 		}
 	}
+
+	/* renders doorss */
+	mvprintw(room->doors[0].y, room->doors[0].x, "+");
+	mvprintw(room->doors[1].y, room->doors[1].x, "+");
+	mvprintw(room->doors[2].y, room->doors[2].x, "+");
+	mvprintw(room->doors[3].y, room->doors[3].x, "+");
 
 	return 0;
 }
@@ -159,9 +185,9 @@ Should be calld once for every iteration of the game loop.
 */
 int renderPlayer(Player * player) {
 	/* prints string at designated y,x location (note: y,x coordinate NOT x,y)*/
-	mvprintw(player->yPos, player->xPos, "@");
+	mvprintw(player->position.y, player->position.x, "@");
 	/* mvprint moves cursor after printing, need to move back using move function */
-	move(player->yPos, player->xPos);
+	move(player->position.y, player->position.x);
 
 	return 0;
 }
@@ -180,33 +206,33 @@ int handleInput(int input, Player * player) {
 		/* move up */
 		case 'W':
 		case 'w':
-			newY = player->yPos - 1;
-			newX = player->xPos;
-			//updatePlayerPosition(player->yPos - 1, player->xPos, player);
+			newY = player->position.y - 1;
+			newX = player->position.x;
+			//updatePlayerPosition(player->position.y - 1, player->position.x, player);
 			break;
 
 		/* move left */
 		case 'A':
 		case 'a':
-			newY = player->yPos;
-			newX = player->xPos - 1;
-			//updatePlayerPosition(player->yPos, player->xPos - 1, player);
+			newY = player->position.y;
+			newX = player->position.x - 1;
+			//updatePlayerPosition(player->position.y, player->position.x - 1, player);
 			break;
 
 		/* move down */
 		case 'S':
 		case 's':
-			newY = player->yPos + 1;
-			newX = player->xPos;
-			//updatePlayerPosition(player->yPos + 1, player->xPos, player);
+			newY = player->position.y + 1;
+			newX = player->position.x;
+			//updatePlayerPosition(player->position.y + 1, player->position.x, player);
 			break;
 
 		/* move right */
 		case 'D':
 		case 'd':
-			newY = player->yPos;
-			newX = player->xPos + 1;
-			//updatePlayerPosition(player->yPos, player->xPos + 1, player);
+			newY = player->position.y;
+			newX = player->position.x + 1;
+			//updatePlayerPosition(player->position.y, player->position.x + 1, player);
 			break;
 
 		default:
@@ -219,22 +245,22 @@ int handleInput(int input, Player * player) {
 }
 
 /*
-This function determines whats in the postion provided at newyPos and
-newyPos.
-@param newyPos the new y pos to check to
-@param newxPos the new x pos to check to
+This function determines whats in the postion provided at newPositionY and
+newPositionY.
+@param newPositionY the new y pos to check to
+@param newPositionX the new x pos to check to
 @param player the player struct
 */
-int checkPosition(int newyPos, int newxPos, Player * player) {
+int checkPosition(int newPositionY, int newPositionX, Player * player) {
 
 	/* determines char at new position and what to do */
-	switch(mvinch(newyPos, newxPos)) {
+	switch(mvinch(newPositionY, newPositionX)) {
 		case '.':
-			updatePlayerPosition(newyPos, newxPos, player);
+			updatePlayerPosition(newPositionY, newPositionX, player);
 			break;
 		default:
 			/* mvinch moves cursor to new pos, need to move back */
-			move(player->yPos, player->xPos);
+			move(player->position.y, player->position.x);
 			break;
 	}
 
@@ -246,14 +272,14 @@ int checkPosition(int newyPos, int newxPos, Player * player) {
 /* 
 This functions updates the player position values and draws a floor tile at the
 last player position.
-@param newyPos the new y pos to update to
-@param newxPos the new x pos to update to
+@param newPositionY the new y pos to update to
+@param newPositionX the new x pos to update to
 @param player the player struct to update
  */
-int updatePlayerPosition(int newyPos, int newxPos, Player * player) {
+int updatePlayerPosition(int newPositionY, int newPositionX, Player * player) {
 
-	mvprintw(player->yPos, player->xPos, ".");
-	player->xPos = newxPos;
-	player->yPos = newyPos;
+	mvprintw(player->position.y, player->position.x, ".");
+	player->position.x = newPositionX;
+	player->position.y = newPositionY;
 	return 0;
 }
